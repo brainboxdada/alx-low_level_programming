bc728569e6731e3c36671d922cb453832b8e2e1b
Opeoluwa Dada <oppyforever@gmail.com>
2022-09-29 15:07:36 -0700

    Task 5-8

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -2,10 +2,11 @@
 
 /**
  * _sqrt_recursion - returns the natural square root of a number
- * @n: number to used
+ * @n: number to be used
  *
  * Return: the square root of n
  */
+
 int _sqrt_recursion(int n)
 {
 	if (n == 1 || n == 0)
@@ -17,9 +18,10 @@ int _sqrt_recursion(int n)
  * _sqrt - returns the square root of a number
  * @n: test number
  * @x: squared number
- * 
+ *
  * Return: the square root of n
  */
+
 int _sqrt(int n, int x)
 {
 	if (n > x / 2)
