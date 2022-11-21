50547c02c67452329c3b63932dc2d505c49d4a59
Opeoluwa Dada <oppyforever@gmail.com>
2022-11-21 04:02:42 -0800

    Task

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -2,12 +2,13 @@
 #include <string.h>
 
 /**
-* add_node - adds a new node at the beginning of a list_t list.
+* add_node - adds a new node at the beginning 
+* 	     of a list_t list.
 * @head: A pointer to the head of the list_t list.
 * @str: The string to be added to the list_t list.
 *
 * Return: if the function fails - NULL.
-*               Otherwise - the address of the new element.
+*            Otherwise - the address of the new element.
 */
 
 list_t *add_node(list_t **head, const char *str)
